d687971d414632649884e2053cb25446622c77fe
amitphalwadiya09 <151755174+amitphalwadiya09@users.noreply.github.com>
2024-02-23 23:29:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 function makeid(l) {
   // write your code here
+	let result=""
+	for(let i=0;i<l;i++)
+		{
+	let ans="ABCDEFGHIJKLMNOPQRSTUVWXZY0123456789qwertyuiopasdfghjklzxcvbnm"
+	let randomindex=Math.floor(Math.random()*l)
+	result=result+ans.charAt(randomindex)
+}
+	return result;
 }
 
 // Do not change the code below.
-const l = prompt("Enter a number.");
-alert(makeid(l));
+// const l = prompt("Enter a number.");
+// alert(makeid(l));
